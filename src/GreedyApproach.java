d76b3665b603c66e8d530d113cb2b799a0f14b19
Mohamed Elansari <elansari1211@gmail.com>
2024-05-04 02:28:26 +0300

    changed some variable names

--- src/GreedyApproach.java
+++ src/GreedyApproach.java
@@ -4,17 +4,17 @@ public class GreedyApproach {
 
     public static void minWalkingDistance(int[] passengers, int maxStops) {
         Arrays.sort(passengers);
-        int elevator = 0;
-        int stopsUsed = 0;
-        int nextPassengerWalk;
-        int afterNextPassengerWalk;
-        int walkingDistance = 0;
+        int elevator;//The floor the elevator at
+        int stopsUsed = 0;//The stopped used
+        int nextPassengerWalk; //Walked floors for the next passenger from the floor the elevator at
+        int afterNextPassengerWalk;//Walked floors for the after next passenger from the floor the elevator at
+        int walkingFloors = 0;//Total walked floors
         int[] stops = new int[maxStops];
         if (maxStops == passengers.length) {
             for (int i = 0; i < passengers.length; i++) {
                 stops[i] = passengers[i];
             }
-            printResult(walkingDistance, stops, maxStops);
+            printResult(walkingFloors, stops, maxStops);
         } else {
             elevator = passengers[0];
             stops[0] = elevator;
@@ -22,11 +22,11 @@ public class GreedyApproach {
 
             for (int i = 1; i < passengers.length; i++) {
                 if (stopsUsed < maxStops){
-                    nextPassengerWalk = passengers[i] - elevator; //dist1=2
-                    afterNextPassengerWalk = passengers[i+1] - elevator;//dist2=13
+                    nextPassengerWalk = passengers[i] - elevator;
+                    afterNextPassengerWalk = passengers[i+1] - elevator;
                     if (nextPassengerWalk < afterNextPassengerWalk) {
                         elevator = passengers[i+1];
-                        walkingDistance += nextPassengerWalk;
+                        walkingFloors += nextPassengerWalk;
                         stops[stopsUsed] = elevator;
                         stopsUsed++;
                         i++;
@@ -37,10 +37,10 @@ public class GreedyApproach {
                     }
                 }else{
                     nextPassengerWalk = passengers[i] - elevator; //dist1=2
-                    walkingDistance += nextPassengerWalk;
+                    walkingFloors += nextPassengerWalk;
                 }
             }
-            printResult(walkingDistance, stops, maxStops);
+            printResult(walkingFloors, stops, maxStops);
         }
     }
 
@@ -53,9 +53,9 @@ public class GreedyApproach {
     }
 
     public static void main(String[] args) {
-        int k = 1;
+        int k = 2;
 
-        int[] passengers = {6, 8, 12,14};
+        int[] passengers = {27,40,60,61};
         minWalkingDistance(passengers, k);
     }
 }
