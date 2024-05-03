79e3b10e3364a206d859c13d576893ce392517df
Mohamed Elansari <elansari1211@gmail.com>
2024-05-03 18:39:21 +0300

    test5

--- src/GreedyApproach.java
+++ src/GreedyApproach.java
@@ -54,6 +54,7 @@ public class GreedyApproach {
 
     public static void main(String[] args) {
         int k = 1;
+
         int[] passengers = {6, 8, 12,14};
         minWalkingDistance(passengers, k);
     }
